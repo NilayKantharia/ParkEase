e98bb796ff0fddde6a3a6f29b6f50eaee43b04da
Nilay <nilaykanthariya@gmail.com>
2024-07-07 19:14:01 +0530

    added route to view all tickets

--- Backend/routes/ticket.js
+++ Backend/routes/ticket.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const router = express.Router();
-const {handleBookTicket, handleViewTicket} = require('../controllers/ticket');
+const router = express.Router({mergeParams : true});
+const {handleBookTicket, handleViewTicket, handleShowAllTicket} = require('../controllers/ticket');
 
 router.post('/book', handleBookTicket);
+router.get('/view', handleShowAllTicket);
 router.get('/view/:id', handleViewTicket);
 
 module.exports = router;
