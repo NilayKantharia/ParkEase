59d12b9a55555e391f7286f092613a717df528ea
Nilay <nilaykanthariya@gmail.com>
2024-07-07 19:15:07 +0530

    created view all ticket and view ticket controllers

--- Backend/controllers/ticket.js
+++ Backend/controllers/ticket.js
@@ -7,10 +7,28 @@ const handleBookTicket = (req, res) => {
 
 //View Ticket Controller
 const handleViewTicket = (req, res) => {
+    const query = "SELECT * FROM ticket_type WHERE ticket_type_id = ?";
+    connection.query(query, [req.params.id], (err, result) => {
+        if(err) {
+            return res.json(err);
+        }
+        return res.json(result);
+    })
+};
 
+//Show all Ticket Controller
+const handleShowAllTicket = (req, res) => {
+    const query = "SELECT * FROM ticket_type";
+    connection.query(query, (err, result) => {
+        if(err) {
+            return res.json(err);
+        }
+        return res.json(result);
+    })
 };
 
 module.exports = {
     handleBookTicket,
-    handleViewTicket
+    handleViewTicket,
+    handleShowAllTicket
 }
