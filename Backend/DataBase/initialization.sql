e3759105e1a497dfa9c888174dec9b632187fa3c
Nilay <nilaykanthariya@gmail.com>
2024-07-08 11:26:48 +0530

    updated initialization

--- Backend/DataBase/initialization.sql
+++ Backend/DataBase/initialization.sql
@@ -4,3 +4,9 @@ INSERT INTO ticket_type (ticket_type_name, ticket_type_price, ticket_type_short_
 INSERT INTO ticket_type (ticket_type_name, ticket_type_price, ticket_type_short_description, ticket_type_description) VALUES ("Silver", 1000, "enjoy half the day", "you can use every ride");
 
 INSERT INTO ticket (ticket_type_id, ticket_price, no_of_members, user_id, booked_for, ticket_scanned_at)VALUES (1, 150.00, 2, 1, '2024-07-10', NULL);  
+
+
+INSERT INTO stall (stall_name, location, description, stall_type) VALUES ("Aditya Dal Bati House", "Near Roller Coster", "Delicious Dal Bati made with pure ghee and love. Try the authenticate taste of Rajasthan.", "Food");
+
+INSERT INTO item (item_name, stall_id, price, image, description, item_type) VALUES ("Dal Bati", 1, 150, "https://www.chitrasfoodbook.com%2F2016%2F07%2Fdal-baati-recipe-rajasthani-dal-bati.html&docid=UvTXsFvplqhJBM&tbnid=LaHqMGV7pkwFpM&vet=12ahUKEwjwlNTM0ZaHAxWon2MGHZnALLgQM3oECGEQAA..i&w=500&h=633&hcb=2&ved=2ahUKEwjwlNTM0ZaHAxWon2MGHZnALLgQM3oECGEQAA", "Dal Bati Made with pure ghee and love.", "Food");
+INSERT INTO item (item_name, stall_id, price, image, description, item_type) VALUES ("Water Bottle", 1, 100, "https://www.chitrasfoodbook.com%2F2016%2F07%2Fdal-baati-recipe-rajasthani-dal-bati.html&docid=UvTXsFvplqhJBM&tbnid=LaHqMGV7pkwFpM&vet=12ahUKEwjwlNTM0ZaHAxWon2MGHZnALLgQM3oECGEQAA..i&w=500&h=633&hcb=2&ved=2ahUKEwjwlNTM0ZaHAxWon2MGHZnALLgQM3oECGEQAA", "Thanda Pani Straight from Rajasthan", "Food");
