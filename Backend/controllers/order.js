891d084ad128dac0ebdc1aa1ccf01e84b800db91
Nilay <nilaykanthariya@gmail.com>
2024-07-08 11:32:22 +0530

    added AllFoodDetails controller

--- Backend/controllers/order.js
+++ Backend/controllers/order.js
@@ -5,6 +5,22 @@ const handleNewOrder = (req, res) => {
 };
 
 const handleAllFoodDetails = (req, res) => {
+    const stallQuery = "SELECT * FROM stall";
+    const itemQuery = "SELECT i.item_id, i.item_name, i.price, i.image, i.description, i.item_type, s.stall_id, s.stall_name, s.location, s.description FROM item i , stall s WHERE (i.stall_id = s.stall_id);"
+    connection.query(stallQuery, (err, result) => {
+        if(err) {
+            return res.json(err);
+        }
+        stallResult = result;
+    });
+
+    connection.query(itemQuery, (err, result) => {
+        if(err) {
+            return res.json(err);
+        }
+        itemResult = result;
+        return res.json({stallResult, itemResult});
+    });
 
 };
 
