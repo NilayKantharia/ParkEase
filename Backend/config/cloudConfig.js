bdcdb25316420ca42ecc2fcba4f8e0ed27a4fc30
Nilay <nilaykanthariya@gmail.com>
2024-07-20 23:44:28 +0530

    minor fixes

--- Backend/config/cloudConfig.js
+++ Backend/config/cloudConfig.js
@@ -1,10 +1,11 @@
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
+require('dotenv').config();
 
 cloudinary.config({
-    cloud_name : 'dbykv4cwf',
-    api_key: '948556546782999',
-    api_secret: 'VTnYClyRi1NqrK9lSbjS_QuqWWM',
+    cloud_name : process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.API_SECRET,
 });
 
 const storage = new CloudinaryStorage({
