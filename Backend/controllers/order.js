8e08a7332892b18c43e9bf42ec78b5fe6ae95452
Nilay <nilaykanthariya@gmail.com>
2024-07-23 20:35:59 +0530

    added order route and controller

--- Backend/controllers/order.js
+++ Backend/controllers/order.js
@@ -2,19 +2,21 @@ const connection = require('../config/connection');
 
 const handleNewOrder = (req, res) => {
     const query = "INSERT INTO `order` (user_id, ticket_id, stall_id) VALUES (?, ?, ?);"
-    const {user_id, ticket_id, stall_id, orderItems} = req.body;
-    connection.query(query, [user_id, ticket_id, stall_id], (err, result) => {
+    const userId = req.userId;
+    const ticketId = req.ticketId;
+    const {stallId, orderItems} = req.body;
+    connection.query(query, [userId, ticketId, stallId], (err, result) => {
         if(err) {
-            return res.json(err);
+            return res.status(400).json(err);
         }
-        const order_id = result.insertId;
+        const orderId = result.insertId;
 
         orderItems.map(item => {
-            const {item_id, quantity} = item;
+            const {itemId, quantity} = item;
             const query = "INSERT INTO order_item (order_id, item_id, quantity) VALUES (?, ?, ?);"
-            connection.query(query, [order_id, item_id, quantity], (err, result) => {
+            connection.query(query, [orderId, itemId, quantity], (err, result) => {
                 if(err) {
-                    return res.json(err);
+                    return res.status(400).json(err);
                 }
             });
         });
@@ -42,7 +44,19 @@ const handleAllFoodDetails = (req, res) => {
 
 };
 
+const handleOrderHistory = (req, res) => {
+    const userId = req.userId;
+    const query = 'SELECT o.order_id, o.order_placed_at, o.order_status, i.item_name, i.price, oi.quantity, s.stall_name FROM `order` o JOIN order_item oi on o.order_id = oi.order_id JOIN item i on oi.item_id = i.item_id JOIN stall s on o.stall_id = s.stall_id WHERE o.user_id = ?;';
+    connection.query(query, [userId], (err, result) => {
+        if(err) {
+            return res.status(400).json(err);
+        }
+        return res.status(200).json(result);
+    })
+}
+
 module.exports = {
     handleNewOrder,
-    handleAllFoodDetails
+    handleAllFoodDetails,
+    handleOrderHistory
 }
--- Backend/routes/order.js
+++ Backend/routes/order.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const {handleNewOrder, handleAllFoodDetails} = require('../controllers/order')
+const {handleNewOrder, handleAllFoodDetails, handleOrderHistory} = require('../controllers/order');
+const {restrictedToLoggedInUsersOnly} = require('../middlewares/auth');
+const {validateTicket} = require('../middlewares/check')
 
 //New Order Route
-router.post('/', handleNewOrder);
+router.post('/new', restrictedToLoggedInUsersOnly, validateTicket, handleNewOrder);
 
 //Get All Food Details
 router.get('/', handleAllFoodDetails);
+
+//Get Order History
+router.get('/history', restrictedToLoggedInUsersOnly, handleOrderHistory);
 
 module.exports = router;
