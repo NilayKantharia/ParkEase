4e1a72c567172bece0022e3ecd37aab40c7c8d95
kdpatel <kdpatel5101@gmail.com>
2024-08-01 15:00:19 +0530

    minor changes

--- Frontend/src/Components/Card.jsx
+++ Frontend/src/Components/Card.jsx
@@ -8,15 +8,15 @@ const Card = ({ image, title, text}) => {
   return (
     <div className="row">
       <div className="col-10 mx-auto">
-        <div className="card mb-5 mt-5">
+        <div className="card mb-5 mt-5 border border-0">
           <div className="row g-0">
-            <div className="col-md-4">
-              <img src={image} className="img-fluid rounded-start explore-image" alt="..." />
+            <div className="col-md-4 col-10 mx-auto">
+              <img src={image} className="img-fluid rounded-3 explore-image " alt="..." />
             </div>
             <div className="col-md-8">
-              <div className="card-body">
-                <h5 className="card-title my-auto">{title}</h5>
-                <p className="card-text mt-auto mb-auto">{text}</p>
+              <div className="card-body mx-3 text-secondary py-0 ps-md-5 pe-0 mt-5 mt-md-0">
+                <h5 className="card-title my-auto mt-0">{title}</h5>
+                <p className="card-text mt-auto mb-auto ">{text}</p>
               </div>
             </div>
           </div>
--- Frontend/src/Components/Card2.jsx
+++ Frontend/src/Components/Card2.jsx
@@ -8,16 +8,16 @@ const Card2 = ({ image, title, text}) => {
   return (
     <div className="row">
       <div className="col-10 mx-auto">
-        <div className="card mb-5 mt-5">
+        <div className="card mb-5 mt-5 border border-0">
           <div className="row g-0">
-            <div className="col-md-8 order-lg-1 order-sm-2 order-2" >
-              <div className="card-body">
+            <div className="col-md-8 order-lg-1 order-sm-2 order-2">
+              <div className="card-body mx-3 text-secondary py-0 ps-0 ps-3 pe-md-5 mt-5 mt-md-0">
                 <h5 className="card-title">{title}</h5>
                 <p className="card-text">{text}</p>
               </div>
             </div>
-            <div className="col-md-4 order-lg-2 order-sm-1 order-1">
-              <img src={image} className="img-fluid rounded-end explore-image" alt="..." />
+            <div className="col-md-4 col-10 mx-auto order-lg-2 order-sm-1 order-1">
+              <img src={image} className="img-fluid rounded-end explore-image rounded-3" alt="..." />
             </div>
           </div>
         </div>
